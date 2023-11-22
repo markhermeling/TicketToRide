a0ff8e9525bf4e07ad0da1f54c95929cb1b60bd1
Canuck-Duck <alex@hermeling.org>
2023-11-21 21:23:25 -0500

    Added all the bridge toll values

--- Board.java
+++ Board.java
@@ -14,58 +14,58 @@ class Board {
                 board[x][y] = 0;
 
         // All Possible Connections Between All The Train Station
-        setBoard(Cities.cities.Emden, Cities.cities.Groningen, 3);
-        setBoard(Cities.cities.Emden, Cities.cities.Lingen, 7);
-        setBoard(Cities.cities.Emden, Cities.cities.Emmen, 4);
-        setBoard(Cities.cities.Groningen, Cities.cities.Emmen, 3);
-        setBoard(Cities.cities.Groningen, Cities.cities.Waddeneilanden,6);
-        setBoard(Cities.cities.Groningen, Cities.cities.Leeuwarden,3);
-        setBoard(Cities.cities.Groningen, Cities.cities.Zwolle,6);
-        setBoard(Cities.cities.Waddeneilanden, Cities.cities.DenHelder,5);
-        setBoard(Cities.cities.Waddeneilanden, Cities.cities.Sneek,3);
-        setBoard(Cities.cities.Waddeneilanden, Cities.cities.Leeuwarden,2);
-        setBoard(Cities.cities.Leeuwarden, Cities.cities.Sneek,1);
-        setBoard(Cities.cities.Sneek, Cities.cities.DenHelder,4);
-        setBoard(Cities.cities.Sneek, Cities.cities.Lelystad,4);
-        setBoard(Cities.cities.DenHelder, Cities.cities.Haarlem,4);
-        setBoard(Cities.cities.Haarlem, Cities.cities.Amsterdam,1);
-        setBoard(Cities.cities.Haarlem, Cities.cities.DenHaag,2);
-        setBoard(Cities.cities.DenHaag, Cities.cities.Rotterdam,1);
-        setBoard(Cities.cities.DenHaag, Cities.cities.Middelburg,6);
-        setBoard(Cities.cities.Middelburg, Cities.cities.Antwerpen,5);
-        setBoard(Cities.cities.Antwerpen, Cities.cities.Rotterdam,5);
-        setBoard(Cities.cities.Antwerpen, Cities.cities.Turnhout,2);
-        setBoard(Cities.cities.Antwerpen, Cities.cities.Aarschot,2);
-        setBoard(Cities.cities.Aarschot, Cities.cities.Hasselt,2);
-        setBoard(Cities.cities.Aarschot, Cities.cities.Liege,5);
-        setBoard(Cities.cities.Liege, Cities.cities.Hasselt,2);
-        setBoard(Cities.cities.Liege, Cities.cities.Maastricht,2);
-        setBoard(Cities.cities.Maastricht, Cities.cities.Hasselt,2);
-        setBoard(Cities.cities.Maastricht, Cities.cities.Eindhoven,4);
-        setBoard(Cities.cities.Maastricht, Cities.cities.Roermond,2);
-        setBoard(Cities.cities.Roermond, Cities.cities.Eindhoven,3);
-        setBoard(Cities.cities.Roermond, Cities.cities.Duisburg,3);
-        setBoard(Cities.cities.Duisburg, Cities.cities.Nijmegen,4);
-        setBoard(Cities.cities.Duisburg, Cities.cities.Enschede,6);
-        setBoard(Cities.cities.Enschede, Cities.cities.Arnhem,5);
-        setBoard(Cities.cities.Enschede, Cities.cities.Zwolle,4);
-        setBoard(Cities.cities.Enschede, Cities.cities.Lingen,3);
-        setBoard(Cities.cities.Lingen, Cities.cities.Emmen,3);
-        setBoard(Cities.cities.Emmen, Cities.cities.Zwolle,4);
-        setBoard(Cities.cities.Zwolle, Cities.cities.Lelystad,2);
-        setBoard(Cities.cities.Zwolle, Cities.cities.Arnhem,4);
-        setBoard(Cities.cities.Lelystad, Cities.cities.Amsterdam,3);
-        setBoard(Cities.cities.Amsterdam, Cities.cities.Rotterdam,4);
-        setBoard(Cities.cities.Amsterdam, Cities.cities.Utrecht,1);
-        setBoard(Cities.cities.Utrecht, Cities.cities.Rotterdam,3);
-        setBoard(Cities.cities.Utrecht, Cities.cities.Arnhem,4);
-        setBoard(Cities.cities.Arnhem, Cities.cities.Nijmegen,1);
-        setBoard(Cities.cities.Nijmegen, Cities.cities.Eindhoven,3);
-        setBoard(Cities.cities.Eindhoven, Cities.cities.DenBosch,1);
-        setBoard(Cities.cities.DenBosch, Cities.cities.Breda,2);
-        setBoard(Cities.cities.Breda, Cities.cities.Rotterdam,2);
-        setBoard(Cities.cities.Breda, Cities.cities.Turnhout,2);
-        setBoard(Cities.cities.Turnhout, Cities.cities.Hasselt,3);
+        setBoard(Cities.cities.Emden, Cities.cities.Groningen, 3,3);
+        setBoard(Cities.cities.Emden, Cities.cities.Lingen, 6,1);
+        setBoard(Cities.cities.Emden, Cities.cities.Emmen, 4,4);
+        setBoard(Cities.cities.Groningen, Cities.cities.Emmen, 3,3);
+        setBoard(Cities.cities.Groningen, Cities.cities.Waddeneilanden,6,4);
+        setBoard(Cities.cities.Groningen, Cities.cities.Leeuwarden,3,2);
+        setBoard(Cities.cities.Groningen, Cities.cities.Zwolle,6,4);
+        setBoard(Cities.cities.Waddeneilanden, Cities.cities.DenHelder,5,4);
+        setBoard(Cities.cities.Waddeneilanden, Cities.cities.Sneek,3,4);
+        setBoard(Cities.cities.Waddeneilanden, Cities.cities.Leeuwarden,2,3);
+        setBoard(Cities.cities.Leeuwarden, Cities.cities.Sneek,1,1);
+        setBoard(Cities.cities.Sneek, Cities.cities.DenHelder,4,3);
+        setBoard(Cities.cities.Sneek, Cities.cities.Lelystad,4,3);
+        setBoard(Cities.cities.DenHelder, Cities.cities.Haarlem,4,2);
+        setBoard(Cities.cities.Haarlem, Cities.cities.Amsterdam,1,1);
+        setBoard(Cities.cities.Haarlem, Cities.cities.DenHaag,2,2);
+        setBoard(Cities.cities.DenHaag, Cities.cities.Rotterdam,1,1);
+        setBoard(Cities.cities.DenHaag, Cities.cities.Middelburg,6,4);
+        setBoard(Cities.cities.Middelburg, Cities.cities.Antwerpen,5,3);
+        setBoard(Cities.cities.Antwerpen, Cities.cities.Rotterdam,5,4);
+        setBoard(Cities.cities.Antwerpen, Cities.cities.Turnhout,2,2);
+        setBoard(Cities.cities.Antwerpen, Cities.cities.Aarschot,2,1);
+        setBoard(Cities.cities.Aarschot, Cities.cities.Hasselt,2,1);
+        setBoard(Cities.cities.Aarschot, Cities.cities.Liege,5,1);
+        setBoard(Cities.cities.Liege, Cities.cities.Hasselt,2,1);
+        setBoard(Cities.cities.Liege, Cities.cities.Maastricht,2,1);
+        setBoard(Cities.cities.Maastricht, Cities.cities.Hasselt,2,2);
+        setBoard(Cities.cities.Maastricht, Cities.cities.Eindhoven,4,2);
+        setBoard(Cities.cities.Maastricht, Cities.cities.Roermond,2,1);
+        setBoard(Cities.cities.Roermond, Cities.cities.Eindhoven,3,3);
+        setBoard(Cities.cities.Roermond, Cities.cities.Duisburg,3,2);
+        setBoard(Cities.cities.Duisburg, Cities.cities.Nijmegen,4,2);
+        setBoard(Cities.cities.Duisburg, Cities.cities.Enschede,6,1);
+        setBoard(Cities.cities.Enschede, Cities.cities.Arnhem,5,2);
+        setBoard(Cities.cities.Enschede, Cities.cities.Zwolle,4,1);
+        setBoard(Cities.cities.Enschede, Cities.cities.Lingen,3,3);
+        setBoard(Cities.cities.Lingen, Cities.cities.Emmen,3,2);
+        setBoard(Cities.cities.Emmen, Cities.cities.Zwolle,4,3);
+        setBoard(Cities.cities.Zwolle, Cities.cities.Lelystad,2,4);
+        setBoard(Cities.cities.Zwolle, Cities.cities.Arnhem,4,2);
+        setBoard(Cities.cities.Lelystad, Cities.cities.Amsterdam,3,3);
+        setBoard(Cities.cities.Amsterdam, Cities.cities.Rotterdam,4,2);
+        setBoard(Cities.cities.Amsterdam, Cities.cities.Utrecht,1,1);
+        setBoard(Cities.cities.Utrecht, Cities.cities.Rotterdam,3,3);
+        setBoard(Cities.cities.Utrecht, Cities.cities.Arnhem,4,1);
+        setBoard(Cities.cities.Arnhem, Cities.cities.Nijmegen,1,3);
+        setBoard(Cities.cities.Nijmegen, Cities.cities.Eindhoven,3,3);
+        setBoard(Cities.cities.Eindhoven, Cities.cities.DenBosch,1,2);
+        setBoard(Cities.cities.DenBosch, Cities.cities.Breda,2,2);
+        setBoard(Cities.cities.Breda, Cities.cities.Rotterdam,2,4);
+        setBoard(Cities.cities.Breda, Cities.cities.Turnhout,2,2);
+        setBoard(Cities.cities.Turnhout, Cities.cities.Hasselt,3,3);
     }
 
     public Board() {
@@ -78,7 +78,7 @@ class Board {
 
 
 
-    public void setBoard(Cities.cities x, Cities.cities y, int value) {
+    public void setBoard(Cities.cities x, Cities.cities y, int value, int BridgeTool) {
         // Used to set the distance between 2 cities
         // First version of this program we will use just the distance. Once we have that working
         // We can switch the int to a class that contains the distance and the colours of the route and cost
